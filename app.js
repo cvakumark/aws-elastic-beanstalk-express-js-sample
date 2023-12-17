1feaf6340b8ca61c394a6d66fcdbb3afd3afc8c6
cvakumark <cvakumark@users.noreply.github.com>
2023-12-17 05:32:10 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World SivaKK!'));
+app.get('/', (req, res) => res.send('Hello World Siva Kumar K!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
